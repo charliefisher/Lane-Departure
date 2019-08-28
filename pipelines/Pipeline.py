320057f6c303341de975e6dda63e2b3ac8a51cea
Charlie Fisher <charliefisherhome@gmail.com>
2019-08-28 00:01:20 -0400

    Working processes for running lane detection algorithm concurrently

--- pipelines/Pipeline.py
+++ pipelines/Pipeline.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-import cv2
+from multiprocessing import Process
 import time
+import cv2
 
 
 class Pipeline(ABC):
@@ -8,11 +9,11 @@ class Pipeline(ABC):
   A superclass for OpenCV lane detection pipelines
 
   It handles opening and closing a video feed and subclasses simply implement the lane detection algorithm in their
-  run() method.
+  _run() method.
 
   Use:
     start() to open video feed and calls run()
-    run() is where the lane detection algorithm is implemented (MUST be overriden by subclass)
+    _run() is where the lane detection algorithm is implemented (MUST be overriden by subclass)
     stop() to close video feed and windows and stop calling run()
 
   :ivar _show_pipeline: a flag indicating whether or not each step in the pipeline should be shown
@@ -21,7 +22,6 @@ class Pipeline(ABC):
   :ivar _capture: the OpenCV capture object (CvCapture) that the lane detection algorithm should run on
   """
 
-  @classmethod
   def __init__(self, source, should_start, show_pipeline, debug):
     """
     Declares instance variables (_show_pipeline, _debug, _capture) and starts the pipeline according to should_start
@@ -32,18 +32,19 @@ class Pipeline(ABC):
     :param debug: a flag indicating whether or not the use is debugging the pipeline. In debug, the pipeline is
                   shown and debug statements are enabled
     """
-
     self.__stop = False
     self.__source = source
     self._show_pipeline = show_pipeline or debug
     self._debug = debug
     self._capture = None
 
+    self.__process = Process(target=self._run_pipeline, args=(self,))
+    self.__process.daemon = False
+
     # check if the pipeline should start immediately
     if should_start:
       self.start()
 
-  @classmethod
   def start(self):
     """
     Opens the video feed and handles the looping of run()
@@ -51,34 +52,10 @@ class Pipeline(ABC):
     :return: void
     """
 
-    # open input
-    self.__open_source(self.__source)
-    # get fps of video
-    fps = self._capture.get(cv2.CAP_PROP_FPS)
-    # loop run() while the capture is open and we we have not stopped running
-    while not self.__stop and self._capture.isOpened():
-      # store start time of loop
-      start_time = time.time()
-      # read a frame of the capture
-      return_value, frame = self._capture.read()
-      # check that the next frame was read successfully
-      # i.e. that we have not hit the end of the video or encountered an error
-      if return_value:
-        self._run(self, frame)
-        # check if 'q' is pressed to stop pipeline
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-          self.stop()
-      else:
-        # stop the pipeline if we hit the end of the video or encountered an error
-        self.stop()
-      # only sleep if stop was not called (i.e. we will read the next frame)
-      if not self.__stop:
-        # 1 second / fps = time to sleep for each frame subtract elapsed time
-        time_to_sleep = max(1 / fps - (time.time() - start_time), 0)
-        time.sleep(time_to_sleep)
+    self.__process.start()
 
   @classmethod
-  def __open_source(self, input):
+  def __open_source(self, capture, input):
     """
     Opens a cv2.capture object
 
@@ -89,22 +66,57 @@ class Pipeline(ABC):
     :return: void
     """
 
-    if not self._capture:
-      self._capture = cv2.VideoCapture(input)
+    if not capture:
+      capture = cv2.VideoCapture(input)
     else:
       raise Exception('Cannot open {input} as a capture is already open'.format(input=input))
+    return capture
 
   @classmethod
-  def stop(self):
+  def _run_pipeline(self, parent):
+    """
+
+    :return: void
+    """
+
+    # open input
+    parent._capture = self.__open_source(parent._capture, parent.__source)
+    # get fps of video
+    fps = parent._capture.get(cv2.CAP_PROP_FPS)
+    # loop run() while the capture is open and we we have not stopped running
+    while not parent.__stop and parent._capture.isOpened():
+      # store start time of loop
+      start_time = time.time()
+      # read a frame of the capture
+      return_value, frame = parent._capture.read()
+      # check that the next frame was read successfully
+      # i.e. that we have not hit the end of the video or encountered an error
+      if return_value:
+        self._run(self, frame)
+        # check if 'q' is pressed to stop pipeline
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          self.stop(parent)
+      else:
+        # stop the pipeline if we hit the end of the video or encountered an error
+        self.stop(parent)
+      # only sleep if stop was not called (i.e. we will read the next frame)
+      if not parent.__stop:
+        # 1 second / fps = time to sleep for each frame subtract elapsed time
+        time_to_sleep = max(1 / fps - (time.time() - start_time), 0)
+        time.sleep(time_to_sleep)
+
+  @classmethod
+  def stop(self, parent):
     """
     Closes _capture and all windows and stops looping run()
 
     :return: void
     """
 
-    self._capture.release()
+    parent._capture.release()
     cv2.destroyAllWindows()
-    self.__stop = True
+    parent.__stop = True
+    # do not have to worry about joining the thread on stop since it is not daemonic and non daemonic threads are joinged automatically
 
   @classmethod
   @abstractmethod
